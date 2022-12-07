e66e87e8159016a37382502d12a694cdff901d89
atakanteko <atakantekoglu@gmail.com>
2022-12-07 23:30:42 +0300

    react suspense

---
+++ src/component/loader/AppLoader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+ 
+export const AppLoader = () => {
+  return (
+    <section className='app-loader'>
+      <div className='bouncing-loader'>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </section>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Suspense } from "react";
+import { AppLoader } from './component/loader/AppLoader';
+
+const App = React.lazy(() => import('./App'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <Suspense fallback={ <AppLoader /> }>
+        <App />
+      </Suspense>
   </React.StrictMode>
 );
 
